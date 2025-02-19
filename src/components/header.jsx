adeea227be021c417b47206c37066edb3ab15db2
THAMIZHSELVAN08 <143354164+THAMIZHSELVAN08@users.noreply.github.com>
2025-02-19 09:11:34 +0530

    Create header.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+  <nav class="bg-white shadow-md">
+        <div class="container mx-auto flex justify-between items-center py-4 px-6">
+            <div  class="flex items-center space-x-3">
+                <img src="login-logo-small.png" alt="Logo" class="h-10 w-auto">
+                <span class="text-lg font-semibold text-gray-700">Report</span>
+
+            </div>
+            <div class="space-x-6">
+                <a href="admin.html" class="text-red-500 hover:text-red-600 font-semibold transition duration-300">Admin Login</a>
+            </div>
+        </div>
+    </nav>  
+}
