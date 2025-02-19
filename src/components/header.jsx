d4282d7c0b8b16d4a909956eb577034682ee7dc3
mubashir-73 <mubashirsheriff7@gmail.com>
2025-02-19 09:33:45 +0530

    create header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
+import logo from "../assets/login-logo-small.png";
 export default function Header() {
   return (
     <nav className="bg-slate-300 shadow-md">
       {" "}
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
         <div className="flex items-center space-x-3">
-          <img src="login-logo-small.png" alt="Logo" className="h-10 w-auto" />
+          <img src={logo} alt="Logo" className="h-10 w-auto" />
           <span className="text-lg font-semibold text-gray-700">Report</span>
         </div>
         <div className="space-x-6">
